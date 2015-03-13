9906ffe69ce1c4f38cfc12348f256cf9731aad04
selbekk <kristofer.selbekk@bekk.no>
2015-03-13 18:28:12 +0100

    Start parallaxing with CSS instead

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,3 @@
 (function() {
-    // Initialize skrollr
-    var s = skrollr.init({
-        render: function(data) {
-            //Debugging - Log the current scroll position.
-            console.log(data.curTop);
-        }
-    });
+    
 })();
