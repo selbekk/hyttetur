ef8f10bfff67379119794ee9d2f6e4e9da270ae9
selbekk <kristofer.selbekk@bekk.no>
2015-03-13 17:19:50 +0100

    Keep a watchful eye out for changes

--- gulpfile.js
+++ gulpfile.js
@@ -10,12 +10,14 @@ var gulp = require('gulp'),
 
 // Clean build
 gulp.task('clean', function(cb) {
+    console.log('deleting src/assets');
 	del(['src/assets'], cb);
 });
 
 
 // CSS build
 gulp.task('style', function () {
+    console.log('compiling styles');
 	return gulp.src('src/css/**.css')
 		.pipe(plumber())
 		.pipe(cssPrefixed({browsers: ['last 2 versions'], cascade: false}))
@@ -26,15 +28,25 @@ gulp.task('style', function () {
 
 // Bower stuff
 gulp.task('bower:wire', function() {
+    console.log('wiring in any new bower dependencies');
 	wiredep({src: './src/*.html', dest: './src/*.html'});
 });
 
 gulp.task('bower:copy', function () {
+    console.log('copying any new bower components');
     gulp.src('bower_components/**')
         .pipe(changed('src/bower_components'))
         .pipe(gulp.dest('src/bower_components'));
 });
 
+// Watch for changes
+gulp.task('watch', function() {
+    console.log('watching for changes...')
+    gulp.watch('src/css', ['clean', 'style']);
+    gulp.watch('bower.json', ['bower']);
+});
+
+// Web server
 gulp.task('serve', function() {
 	gulp.src('src')
 		.pipe(server({
@@ -45,4 +57,4 @@ gulp.task('serve', function() {
 
 // Combos
 gulp.task('bower', ['bower:wire', 'bower:copy']);
-gulp.task('default', ['bower', 'clean', 'style', 'serve']);
+gulp.task('default', ['bower', 'clean', 'style', 'serve', 'watch']);
