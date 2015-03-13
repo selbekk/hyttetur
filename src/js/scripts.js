5e1ded578bfb4647409642d96029568e277ccdb3
selbekk <kristofer.selbekk@bekk.no>
2015-03-13 16:33:40 +0100

    add skrollr basics

---
+++ src/js/scripts.js
@@ -0,0 +1,9 @@
+(function() {
+    // Initialize skrollr
+    var s = skrollr.init({
+        render: function() {
+            //Debugging - Log the current scroll position.
+            console.log(data.curTop);
+        }
+    });
+})();
