1a2445774099bc1601f47b6e38c75b4e4fb1a8eb
selbekk <kristofer.selbekk@bekk.no>
2015-03-13 17:42:15 +0100

    add a second div

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,7 +1,7 @@
 (function() {
     // Initialize skrollr
     var s = skrollr.init({
-        render: function() {
+        render: function(data) {
             //Debugging - Log the current scroll position.
             console.log(data.curTop);
         }
