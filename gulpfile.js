b365ed78fa4786c55de4302fa72affbcb45c13ca
selbekk <kristofer.selbekk@bekk.no>
2015-03-13 16:50:13 +0100

    Copy dependencies with gulp-changed filter

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
 var gulp = require('gulp'),
     wiredep = require('wiredep'),
-    server = require('gulp-server-livereload');
+    server = require('gulp-server-livereload'), 
+    changed = require('gulp-changed');
 
-gulp.task('bower', function() {
+gulp.task('bower', ['bower:wire', 'bower:copy']);
+
+gulp.task('bower:wire', function() {
 	wiredep({src: './src/*.html', dest: './src/*.html'});
+});
+
+gulp.task('bower:copy', function () {
+    gulp.src('bower_components/**')
+        .pipe(changed('src/bower_components'))
+        .pipe(gulp.dest('src/bower_components'));
 });
 
 gulp.task('serve', function() {
@@ -13,3 +22,5 @@ gulp.task('serve', function() {
 			open: true
 		}));
 });
+
+gulp.task('default', ['bower', 'serve']);
