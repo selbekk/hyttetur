ff7a4e2615db0dc1245469064aa9a40e5f076e30
selbekk <kristofer.selbekk@bekk.no>
2015-03-13 16:40:08 +0100

    add development webserver

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 var gulp = require('gulp'),
-    wiredep = require('wiredep');
+    wiredep = require('wiredep'),
+    server = require('gulp-server-livereload');
 
 gulp.task('bower', function() {
 	wiredep({src: './src/*.html', dest: './src/*.html'});
+});
+
+gulp.task('serve', function() {
+	gulp.src('src')
+		.pipe(server({
+			liveReload: false,
+			open: true
+		}));
 });
