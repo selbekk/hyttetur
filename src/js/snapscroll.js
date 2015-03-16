5bf6b880eb788a06fd950a76aa9f74a63500a590
selbekk <kristofer.selbekk@bekk.no>
2015-03-16 19:50:09 +0100

    Add snap scroll

---
+++ src/js/snapscroll.js
@@ -0,0 +1,10 @@
+(function() {
+    $('.parallax').panelSnap({
+        panelSelector: '.parallax-group',
+        directionThreshold: 200,
+        easing: 'swing',
+        keyboardNavigation: {
+            enabled: true
+        }
+    });
+})();
