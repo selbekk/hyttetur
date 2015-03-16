6ea4d48b2642fb6da389cb1db4785e727fed8378
selbekk <kristofer.selbekk@bekk.no>
2015-03-16 20:14:06 +0100

    Wire up menu to snapScroll

--- src/js/snapscroll.js
+++ src/js/snapscroll.js
@@ -1,5 +1,6 @@
 (function() {
     $('.parallax').panelSnap({
+        $menu: $('.navigation-links'),
         panelSelector: '.parallax-group',
         directionThreshold: 200,
         easing: 'swing',
