11ac1dd2d381849d4e1433717a06fdedd436af77
selbekk <kristofer.selbekk@bekk.no>
2015-03-16 23:31:45 +0100

    Add feedback for mails

--- src/js/idea.js
+++ src/js/idea.js
@@ -16,12 +16,31 @@
     }
 
     function mailSent(e) {
-        console.log('mail sent', e);
+        var template = '<div class="mail-response mail-response-success">' +
+                            '<span class="feedback glyphicon glyphicon-ok shake"></span>' +
+                            '<h2>Takk for forslaget!</h2>' +
+                            '<p>Det skal vi <em>virkelig vurdere</em></p>' +
+                        '</div>';
+
+        updateView(template);
     }
 
     function mailFailed(a, b, c) {
         console.log('mail failed to send', a, b, c);
+        var template = '<div class="mail-response mail-response-error">' +
+                            '<span class="feedback glyphicon glyphicon-thumbs-down shake"></span>' +
+                            '<h2>Kjipern!</h2>' +
+                            '<p>Noe gikk feil! Da driter vi i idéen din. Sikkert ikke no bra uansett.</p>' +
+                        '</div>';
 
+        updateView(template);
+    }
+
+    function updateView(template) {
+        var form = $('#idea-form');
+        form.fadeOut(500, function() {
+            form.html(template).fadeIn(500);
+        });
     }
 
     $('#idea-form').on('submit', submitForm);
